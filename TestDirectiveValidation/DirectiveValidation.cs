4998e75ef697171457018b97222f9c5c2876cfae
Thomas May <tjmay@inscico.com>
2024-11-27 15:16:12 -0800

    a better test harness, centering on 1 to 1 test case, test method, test file relationship. Machine agnostic file reference implmented

--- TestDirectiveValidation/DirectiveValidation.cs
+++ TestDirectiveValidation/DirectiveValidation.cs
@@ -1,5 +1,6 @@
 using LazyMagic;
 using NJsonSchema.Validation;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TestDirectiveValidation
 {
@@ -10,31 +11,37 @@ namespace TestDirectiveValidation
         public void Error(Exception ex, string message) { }
         public Task ErrorAsync(Exception ex, string message) => Task.CompletedTask;
     }
+
     public class DirectiveValidation
     {
+        public ILogger testLogger { get; set; }
+        string basePath;
+        string testPath;
+
+        public DirectiveValidation()
+        {
+            testLogger = new TestLogger();
+            basePath = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
+            testPath = Path.Combine(basePath, "TestDirectiveValidation", "DirectiveFiles");
+        }
 
         [Fact]
+        /*our naming convention has led to some awkward variable names in this case. Be not confused. The configuration file which acts as the 
+         single source of truth from which lazy magic generates is called the DIRECTIVE FILE. Here we test the vailidity of the DIRECTIVE FILE
+         by running the validate() method in each artifcat class contained therin. The sample directive files wich will throw errors are things like 
+         TestFile1.yaml or TestFile2.yaml. Which is why we need a DIRECITVE file, filePath.
+
+         Although contianing many directives, we use the singular DIRECTIVE throughout. 
+         */
         public async Task TestDirectiveValidationAsync()
         {
+            string directiveFileFilePath = Path.Combine(testPath, "TestFile1.yaml");
 
-            var logger = new TestLogger();
-            var solutionRootFolderPath = @"C:\Users\Conda\source\repos\_Dev\LazyMagic\LazyMagicMDD";
-            var projectWithTestFiles = "TestDirectiveValidation";
-            var folderWithTestFiles = "DirectiveFiles";
-            var testDir = Path.Combine(solutionRootFolderPath, projectWithTestFiles, folderWithTestFiles);
-            var files = Directory.GetFiles(testDir);
-
-            foreach (string directiveFilePath in Directory.GetFiles(testDir))
+            await Assert.ThrowsAsync<Exception>(async () =>
             {
-
-                var lzSolution = new LzSolution(logger, solutionRootFolderPath);
-
-                await Assert.ThrowsAsync<Exception>(async () =>
-                {
-                    await lzSolution.TestDirectiveValidation(directiveFilePath);
-                    throw new Exception($"Sample error message: {directiveFilePath}");
-                });
-            }
+                await new LzSolution(testLogger, basePath).TestDirectiveValidation(directiveFileFilePath);
+                throw new Exception($"Sample error message: {directiveFileFilePath}");
+            });
         }
     }
 }
